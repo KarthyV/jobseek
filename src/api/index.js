585b78294f33aa557933d671aec07eb951373a99
KarthyV <Kdon979@gmail.com>
2022-09-16 17:13:32 +0530

    server change

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-// const URL = "https://tolemy-task.herokuapp.com";
+const URL = "https://tolemy-task.herokuapp.com";
 
-const URL = "http://localhost:8000";
+// const URL = "http://localhost:8000";
 
 export default axios.create({
   baseURL: URL,
